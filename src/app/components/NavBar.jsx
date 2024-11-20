7d7a50ea41d899be48dc9357ce33e075ed2fb1f3
JanivDemagniv <janivjry@gmail.com>
2024-11-20 11:56:25 +0200

    profile picture for the navbar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -5,14 +5,19 @@ import Button from './Button'
 export default function NavBar({ }) {
     return (
         <nav className={styles.nav_bar}>
-            <span><span className={styles.backColor}>Yaniv</span> Romem</span>
-            <ul>
-                <li><Button to='/' label='Home' /></li>
-                <li><Button to='/about' label='About' /></li>
-                <li><Button to='/projects' label='Projects' /></li>
-                <li><Button to='/gamecenter' label='Game Center' /></li>
-                <li><Button to='/contact' label='Contact Me' /></li>
-            </ul>
+            <div className={styles.logo}>
+                <div className={styles.profilePic}><img src="./protfolioPic.jpg" alt="Profile Picture" /></div>
+                <span><span className={styles.backColor}>Yaniv</span> Romem</span>
+            </div>
+            <div>
+                <ul>
+                    <li><Button to='/' label='Home' /></li>
+                    <li><Button to='/about' label='About' /></li>
+                    <li><Button to='/projects' label='Projects' /></li>
+                    <li><Button to='/gamecenter' label='Game Center' /></li>
+                    <li><Button to='/contact' label='Contact Me' /></li>
+                </ul>
+            </div>
         </nav>
     )
 }
