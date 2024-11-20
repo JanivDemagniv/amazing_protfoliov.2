d15a9bc8976c8d9ef7098f4d784b8a46ef9b35d8
JanivDemagniv <janivjry@gmail.com>
2024-11-20 12:51:36 +0200

    addin tooltips for the skills display

--- src/app/components/Technolgy.jsx
+++ src/app/components/Technolgy.jsx
@@ -3,11 +3,16 @@ import styles from './styles/Technolgy.module.css'
 
 export default function Technolgy({ skills }) {
     return (
-        <div className={styles.skillsLine}>
+        <div>
             <h6>My Skills:</h6>
-            {skills.map((skill) => {
-                return <div className={styles.skillClass}><img src={skill.imgUrl} alt={skill.imgAlt} /></div>
-            })}
+            <div className={styles.skillsLine}>
+                {skills.map((skill) => {
+                    return <div className={styles.skillClass}>
+                        <img src={skill.imgUrl} alt={skill.imgAlt} />
+                        <span className={styles.toolTip}>{skill.name}</span>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
--- src/app/data/skills.js
+++ src/app/data/skills.js
@@ -1,65 +1,86 @@
 const skills = [
     {
+        name: 'HTML',
         imgUrl: './skills/HTML.svg',
         imgAlt: 'HTML'
     },
     {
+        name: 'CSS',
         imgUrl: './skills/css.svg',
         imgAlt: 'css'
     },
     {
+        name: 'SASS',
         imgUrl: './skills/SASS.png',
         imgAlt: 'SASS'
     },
     {
+        name: 'JavaScript',
         imgUrl: './skills/javascript.svg',
         imgAlt: 'javascript'
     },
     {
+        name: 'ES6',
         imgUrl: './skills/ES6.png',
         imgAlt: 'ES6'
     },
     {
+        name: 'Object-Oriented Programming',
         imgUrl: './skills/OOP.png',
         imgAlt: 'OOP'
     },
     {
+        name: 'TypeScript',
         imgUrl: './skills/TypeScript.png',
         imgAlt: 'TypeScript'
     },
     {
+        name: 'React',
         imgUrl: './skills/react.svg',
         imgAlt: 'react'
     },
     {
-        imgUrl: './skills/reactrouter.svg',
-        imgAlt: 'reactrouter'
-    },
-    {
+        name: 'Material UI',
         imgUrl: './skills/mui.svg',
         imgAlt: 'mui'
     },
     {
+        name: 'Vite',
         imgUrl: './skills/vite.svg',
         imgAlt: 'vite'
     },
     {
+        name: 'Next JS',
         imgUrl: './skills/nextjs.svg',
         imgAlt: 'nextjs'
     },
     {
+        name: 'Node JS',
         imgUrl: './skills/nodejs.svg',
         imgAlt: 'nodejs'
     },
     {
+        name: 'MySql',
+        imgUrl: './skills/mysql.svg',
+        imgAlt: 'mysql'
+    },
+    {
+        name: 'Mongo Db',
+        imgUrl: './skills/mongodb.svg',
+        imgAlt: 'mongodb'
+    },
+    {
+        name: 'Mongoose',
         imgUrl: './skills/mongoose.svg',
         imgAlt: 'mongoose'
     },
     {
+        name: 'Express JS',
         imgUrl: './skills/express.svg',
         imgAlt: 'express'
     },
     {
+        name: 'JSON Web Token',
         imgUrl: './skills/jwt.svg',
         imgAlt: 'jwt'
     },
